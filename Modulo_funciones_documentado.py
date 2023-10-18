e05dcc1e0a3580e928e271e7284f1460adf6af45
Ivan Estrada <jestradar@ceduc.cl>
2023-10-18 10:59:16 -0300

    Comentario de la primer version

---
+++ Modulo_funciones_documentado.py
@@ -0,0 +1,27 @@
+
+#Autor(es): Iván Estrada
+#Fecha: 27/09/23
+#Descripcion:Este modulo tiene las cuatro operaciones
+        #matematicas básicas.
+
+def sumar(num1, num2):
+    """ Esta funcion recibe dos numeros y devuelve la suma de ambos """
+    return num1+num2
+
+def restar(num1, num2):
+    return num1-num2
+
+def dividir(num1, num2):
+    return num1/num2
+
+def multiplicar(num1, num2):
+    return num1*num2
+
+help(sumar)
+help(restar)
+help(dividir)
+help(multiplicar)
+
+
+
+
